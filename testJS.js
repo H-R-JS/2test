39fd55d70bfa2cd9c729b600dc1e89b34312faf6
WhitRavn <jmr4277@orange.fr>
2022-06-06 16:31:25 +0200

    end first test

--- testJS.js
+++ testJS.js
@@ -4,22 +4,33 @@ button.addEventListener("click", function () {
   this.nextElementSibling.classList.add("visible");
   this.parentNode.removeChild(this);
 }); */
+(function () {
+  const elementSpoilS = document.querySelectorAll(".spoiler");
 
-const elementSpoilS = document.querySelectorAll(".spoiler");
+  const createSpoilerbtn = function (elementSpoilS) {
+    // On créer le btn
+    const btn = document.createElement("button");
+    btn.textContent = "Afficher le spoiler";
+    // on créer le span
+    const span = document.createElement("span");
+    span.className = "spoiler-content";
+    span.innerHTML = elementSpoilS.innerHTML;
+    // on ajoute au DOM
+    elementSpoilS.innerHTML = "";
+    elementSpoilS.appendChild(btn);
+    elementSpoilS.appendChild(span);
 
-const createSpoilerbtn = function (elementSpoilS) {
-  // On créer le btn
-  const btn = document.createElement("button");
-  btn.textContent = "Afficher le spoiler";
-  // on créer le span
-  const span = document.createElement("span");
-  span.className = "spoiler-content";
-  span.innerHtml = elementSpoilS.innerHtml;
-  // on ajoute au DOM
-  elementSpoilS.appendChild(btn);
-  elementSpoilS.appendChild(span);
-};
+    btn.addEventListener("click", () => {
+      if (btn.textContent == "Afficher le spoiler") {
+        btn.textContent = "Tu es sûr ?";
+      } else {
+        span.classList.add("visible");
+        elementSpoilS.removeChild(btn);
+      }
+    });
+  };
 
-for (var i = 0; i < elementSpoilS.length; i++) {
-  createSpoilerbtn(elementSpoilS[i]);
-}
+  for (var i = 0; i < elementSpoilS.length; i++) {
+    createSpoilerbtn(elementSpoilS[i]);
+  }
+})();
