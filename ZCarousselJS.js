8518ddece9dd2cb50669321cd5400fec8263a653
WhitRavn <jmr4277@orange.fr>
2022-06-29 17:56:24 +0200

    add content JS/CSS

--- ZCarousselJS.js
+++ ZCarousselJS.js
@@ -20,10 +20,11 @@ class Carousel {
       options
     );
     let children = [].slice.call(element.children); // variable array contenant les element enfants ( slice transfert les enfants dans le nouveau tableau, en l'appelant dans le tableau créer et non celui de slice)
-    let root = this.createDivWithClass("carousel");
+    this.currentItem = 0;
+    this.root = this.createDivWithClass("carousel");
     this.container = this.createDivWithClass("carousel-container");
-    root.appendChild(this.container);
-    this.element.appendChild(root);
+    this.root.appendChild(this.container);
+    this.element.appendChild(this.root);
     this.items = children.map((child) => {
       let item = this.createDivWithClass("carousel-item");
       item.appendChild(child);
@@ -31,11 +32,48 @@ class Carousel {
       return item; // créer un div de item
     });
     this.setStyle(); // appel la fonction de calcul d'emplacement
+    this.createNavigation();
   }
+
+  createNavigation() {
+    let nextButton = this.createDivWithClass("carousel-next");
+    let prevButton = this.createDivWithClass("carousel-prev");
+    this.root.appendChild(nextButton);
+    this.root.appendChild(prevButton);
+    nextButton.addEventListener("click", this.next.bind(this));
+    prevButton.addEventListener("click", this.prev.bind(this));
+  }
+
+  next() {
+    this.gotoItem(this.currentItem + this.options.slidesToScroll); // appel gotoItem et calcul en paramètre l'index par rapport à celui initialisé
+  }
+
+  prev() {
+    this.gotoItem(this.currentItem - this.options.slidesToScroll);
+  }
+
+  gotoItem(index) {
+    if (index < 0) {
+      index = this.items.length - this.options.slidesVisible;
+    } else if (
+      index >= this.items.length ||
+      this.items[this.currentItem + this.options.slidesVisible] === undefined
+    ) {
+      index = 0;
+    }
+    let translateX = (index * -100) / this.items.length; // cacule le déplacement en fonction de l'index donné
+    this.container.style.transform = "translate3d(" + translateX + "%, 0, 0)";
+    this.currentItem = index;
+    console.log(index);
+  }
+
+  /**
+   * Applique les bonnes dimensions aux élements du carousel
+   */
 
   setStyle() {
     let ratio = this.items.length / this.options.slidesVisible; // Calcule le fait que enfant soit 3 devant nous en prenant la place adéquat
-    this.container.style.width = ratio * 100 + "%";
+    this.container.style.width = ratio * 100 + "%"; // en pourcentage pour l'adaptation automatique à la fenêtre
     this.items.forEach(
       (item) =>
         (item.style.width = 100 / this.options.slidesVisible / ratio + "%") // Pour chaque item le calcul de son emplacement dans l'itération
@@ -57,7 +95,7 @@ class Carousel {
 
 document.addEventListener("DOMContentLoaded", function () {
   new Carousel(document.querySelector("#carousel1"), {
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     slidesVisible: 3,
   });
 });
