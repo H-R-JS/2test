57c4d706635a44360b6db15970954378c0561d55
WhitRavn <jmr4277@orange.fr>
2022-08-03 17:01:59 +0200

    finishing Scroll Spy

--- ScrollSpyJS.js
+++ ScrollSpyJS.js
@@ -1,16 +1,23 @@
+const ratio = 0.6;
+let Observer = null;
+
 const activate = function (element) {
   const id = element.getAttribute("id");
-  const anchor = document.querySelector("a[href='#" + id + "']");
+  const anchor = document.querySelector("a[href='#" + id + "']"); // L'ancre sur le menu
 
   if (anchor === null) {
     return null;
   }
+  anchor.parentElement
+    .querySelectorAll(".active")
+    .forEach((node) => node.classList.remove("active"));
   anchor.classList.add("active");
 
   console.log(anchor);
 };
 
-const callBack = function (entries, observer) {
+const callBack = function (entries) {
+  // calback qui va dans le param de Observer
   entries.forEach(function (entry) {
     if (entry.intersectionRatio > 0) {
       activate(entry.target);
@@ -20,9 +27,41 @@ const callBack = function (entries, observer) {
 
 const spies = document.querySelectorAll("[data-spy]");
 
-if (spies.length > 0) {
-  const Observer = new IntersectionObserver(callBack, {});
-  spies.forEach(function (spy) {
-    Observer.observe(spy);
+const observe1 = function (elems) {
+  console.log("yes");
+  if (Observer !== null) {
+    elems.forEach((elem) => Observer.unobserve(elem));
+  }
+  const y = Math.round(window.innerHeight * ratio);
+  Observer = new IntersectionObserver(callBack, {
+    rootMargin: `-${window.innerHeight - y - 1}px 0px -${y}px 0px`, // permet de voir section 1 dans le premier calcul
   });
+  spies.forEach((elem) => Observer.observe(elem));
+};
+
+const debounce = function (callback, delay) {
+  let timer;
+  return function () {
+    let args = arguments;
+    let context = this;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      callback.apply(context, args);
+    }, delay);
+  };
+};
+
+if (spies.length > 0) {
+  observe1(spies);
+  let windowH = window.innerHeight;
+  window.addEventListener(
+    "resize",
+    debounce(function () {
+      if (window.innerHeight !== windowH) {
+        console.log("test");
+        observe1(spies);
+        windowH = window.innerHeight;
+      }
+    }, 500)
+  );
 }
